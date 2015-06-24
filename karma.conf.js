325b21e920b983563b55427fcf696f8f9be9aa31
Loïc Mahieu <mahieuloic@gmail.com>
2015-06-24 13:30:27 +0200

    Travis tests on SauceLab

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,37 @@
 'use strict';
 
 module.exports = function(karma) {
+  var customLaunchers = {
+    sl_chrome: {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'Windows 7',
+      version: '35'
+    },
+    sl_firefox: {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      version: '30'
+    },
+    sl_ios_safari: {
+      base: 'SauceLabs',
+      browserName: 'iphone',
+      platform: 'OS X 10.9',
+      version: '7.1'
+    },
+    sl_ie_11: {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 8.1',
+      version: '11'
+    }
+  };
+
   karma.set({
+    sauceLabs: {
+      testName: 'WebSQL Tests'
+    },
+    customLaunchers: customLaunchers,
 
     frameworks: [ 'jasmine', 'browserify' ],
 
@@ -16,7 +46,10 @@ module.exports = function(karma) {
     },
 
     browsers: (
-      process.env.BROWSERS.match(/\w+/g) || [ 'Chrome', 'Safari', 'PhantomJS' ]
+      process.env.BROWSERS.match(/^Sauce/) ? Object.keys(customLaunchers) : (
+        process.env.BROWSERS.match(/\w+/g) ||
+        [ 'Chrome', 'Safari', 'PhantomJS' ]
+      )
     ),
 
     singleRun: false,
